4f0be2093e1aa95ec44f83852afa6580769226e9
Brad Teague <b_rad88@live.com>
2022-07-15 16:35:05 +0930

    Handle current location in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,61 @@
-const fetchBtn = $("#fetchWeather");
-const fetchLocationBtn = $("#fetchLocation");
+// const fetchBtn = $("#fetchWeather");
+// const fetchLocationBtn = $("#fetchLocation");
 const locationInput = $("#locationInput");
-const APP_ID = "a85920815477cd95ada1b55d26549d09";
-const locations = loadLocations();
 const locationBtnContainer = $("#locationBtnContainer");
+const searchForm = $("#searchForm");
 
-fetchLocationBtn.on("click", () => {
-  const location = locationInput.val();
-  fetchLocation(location);
+const APP_ID = "a85920815477cd95ada1b55d26549d09";
+
+const locations = loadLocations();
+// let currentLocation = loadCurrentLocation(); //either the top location or undefined
+
+searchForm.on("submit", async e => {
+  e.preventDefault();
+  const input = $(e.target).find("input");
+  await onSearch(input.val());
 });
+
+async function onSearch(locationName) {
+  const existingLocation = locations.find(
+    location => location.name.toLowerCase() === locationName.toLowerCase()
+  );
+  if (existingLocation) {
+    saveCurrentLocation(existingLocation);
+  } else {
+    try {
+      const location = await fetchLocation(locationName);
+      normaliseLocation(location);
+      saveLocation(location);
+      saveCurrentLocation(location);
+    } catch (error) {
+      // Handle the case where can't find a location
+    }
+  }
+  renderLocations();
+  fetchWeather();
+}
 
 locationBtnContainer.on("click", "button", e => {
   const btn = $(e.target);
-  const location = btn.data("location");
-  fetchWeather(location);
+  currentLocation = btn.data("location");
+  saveCurrentLocation(currentLocation);
+  const weather = fetchWeather();
+  renderLocations();
 });
 
 function loadLocations() {
   const data = JSON.parse(localStorage.getItem("locations"));
   return data || [];
+}
+
+function loadCurrentLocation() {
+  const data = JSON.parse(localStorage.getItem("currentLocation"));
+  // if (!data) return locations[0];
+  return data;
+}
+
+function saveCurrentLocation(location) {
+  localStorage.setItem("currentLocation", JSON.stringify(location));
 }
 
 async function fetchLocation(location) {
@@ -32,50 +69,56 @@ async function fetchLocation(location) {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    saveLocation(data[0]);
-    renderLocations();
+    return data[0];
   } catch (error) {
     console.error(error);
   }
 }
 
-async function fetchWeather(location) {
-  const query = new URLSearchParams(location);
+async function fetchWeather() {
+  const query = new URLSearchParams(currentLocation);
   query.append("appid", "a85920815477cd95ada1b55d26549d09");
   query.append("units", "metric");
   query.append("exclude", "minutely,hourly,alerts");
 
   const url = new URL("data/2.5/onecall", "https://api.openweathermap.org");
   url.search = query.toString();
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data[0];
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function normaliseLocation() {
+  // Only want the following properties from the returned api object
+  currentLocation = {
+    name: currentLocation.name,
+    lat: currentLocation.lat,
+    lon: currentLocation.lon,
+    country: currentLocation.country,
+  };
 }
 
 function saveLocation(location) {
-  if (locationExists(location)) return;
-  const saveItem = {
-    name: location.name,
-    lat: location.lat,
-    lon: location.lon,
-    country: location.country,
-  };
-  locations.push(saveItem);
+  locations.push(location);
   localStorage.setItem("locations", JSON.stringify(locations));
 }
 
-function locationExists(location) {
-  const exists = locations.find(l => l.name === location.name);
-  return !!exists;
-}
-
 function renderLocations() {
+  const currentLocation = loadCurrentLocation();
   locationBtnContainer.html("");
   locations.forEach(location => {
+    console.log(location);
     const btn = $(
-      `<button class="btn btn-success p-3 m-2 text-white fs-4">${location.name}</button>`
+      `<button class="btn btn-primary p-3 m-2 text-white fs-4">${location.name}</button>`
     );
     btn.data("location", location);
+    if (currentLocation && currentLocation.name === location.name) {
+      btn.addClass("btn-success");
+    }
     locationBtnContainer.append(btn);
   });
 }
