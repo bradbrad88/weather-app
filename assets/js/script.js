aeacbe7b37ce2d33e6585f6e5987dc5d8dfde860
Brad Teague <b_rad88@live.com>
2022-07-15 10:09:53 +0930

    Fetch weather event delegation on location buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,16 @@ const fetchLocationBtn = $("#fetchLocation");
 const locationInput = $("#locationInput");
 const APP_ID = "a85920815477cd95ada1b55d26549d09";
 const locations = loadLocations();
+const locationBtnContainer = $("#locationBtnContainer");
 
 fetchLocationBtn.on("click", () => {
   const location = locationInput.val();
   fetchLocation(location);
 });
 
-fetchBtn.on("click", () => {
-  const location = {
-    lat: "123",
-    lon: "456",
-  };
+locationBtnContainer.on("click", "button", e => {
+  const btn = $(e.target);
+  const location = btn.data("location");
   fetchWeather(location);
 });
 
@@ -30,9 +29,27 @@ async function fetchLocation(location) {
   const query = new URLSearchParams(params);
   const url = new URL("geo/1.0/direct", "http://api.openweathermap.org");
   url.search = query.toString();
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    saveLocation(data[0]);
+    renderLocations();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function fetchWeather(location) {
+  const query = new URLSearchParams(location);
+  query.append("appid", "a85920815477cd95ada1b55d26549d09");
+  query.append("units", "metric");
+  query.append("exclude", "minutely,hourly,alerts");
+
+  const url = new URL("data/2.5/onecall", "https://api.openweathermap.org");
+  url.search = query.toString();
   const response = await fetch(url);
   const data = await response.json();
-  saveLocation(data[0]);
+  console.log(data);
 }
 
 function saveLocation(location) {
@@ -51,3 +68,20 @@ function locationExists(location) {
   const exists = locations.find(l => l.name === location.name);
   return !!exists;
 }
+
+function renderLocations() {
+  locationBtnContainer.html("");
+  locations.forEach(location => {
+    const btn = $(
+      `<button class="btn btn-success p-3 m-2 text-white fs-4">${location.name}</button>`
+    );
+    btn.data("location", location);
+    locationBtnContainer.append(btn);
+  });
+}
+
+function init() {
+  renderLocations();
+}
+
+init();
