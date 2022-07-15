936cbf60b55c4159e633a27a4308856965f075c4
Brad Teague <b_rad88@live.com>
2022-07-15 09:47:23 +0930

    Use global locations array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const fetchBtn = $("#fetchWeather");
 const fetchLocationBtn = $("#fetchLocation");
 const locationInput = $("#locationInput");
+const APP_ID = "a85920815477cd95ada1b55d26549d09";
+const locations = loadLocations();
 
 fetchLocationBtn.on("click", () => {
   const location = locationInput.val();
@@ -15,37 +17,37 @@ fetchBtn.on("click", () => {
   fetchWeather(location);
 });
 
+function loadLocations() {
+  const data = JSON.parse(localStorage.getItem("locations"));
+  return data || [];
+}
+
 async function fetchLocation(location) {
   const params = {
     q: location,
-    appid: "a85920815477cd95ada1b55d26549d09",
+    appid: APP_ID,
   };
   const query = new URLSearchParams(params);
   const url = new URL("geo/1.0/direct", "http://api.openweathermap.org");
   url.search = query.toString();
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
-  saveLocation(data);
+  saveLocation(data[0]);
 }
 
 function saveLocation(location) {
   if (locationExists(location)) return;
-  console.log("location doesn't exist");
-  const saveItem = location.map(l => ({
-    name: l.name,
-    lat: l.lat,
-    lon: l.lon,
-    country: l.country,
-  }));
-  const data = JSON.parse(localStorage.getItem("locations")) || [];
-  data.push(saveItem);
-  localStorage.setItem("locations", JSON.stringify(data));
+  const saveItem = {
+    name: location.name,
+    lat: location.lat,
+    lon: location.lon,
+    country: location.country,
+  };
+  locations.push(saveItem);
+  localStorage.setItem("locations", JSON.stringify(locations));
 }
 
 function locationExists(location) {
-  const data = JSON.parse(localStorage.getItem("locations"));
-  if (!data) return false;
-  const exists = data.find(l => l.name === location.name);
+  const exists = locations.find(l => l.name === location.name);
   return !!exists;
 }
